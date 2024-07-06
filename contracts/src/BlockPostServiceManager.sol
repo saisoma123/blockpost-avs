b9a70ce6d7a3e2f44e43f4dd67367f4563328c66
saisoma123 <suryasai.somayyajula@gmail.com>
2024-07-05 22:47:40 -0500

    Added EigenLayer integration for the contract

--- contracts/src/BlockPostServiceManager.sol
+++ contracts/src/BlockPostServiceManager.sol
@@ -1,14 +1,51 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13^;
+
+import "@eigenlayer/contracts/libraries/BytesLib.sol";
+import "@eigenlayer/contracts/core/DelegationManager.sol";
+import "@eigenlayer-middleware/src/unaudited/ECDSAServiceManagerBase.sol";
+import "@eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
+import "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
+import "@eigenlayer/contracts/permissions/Pausable.sol";
+import {IRegistryCoordinator} from "@eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
+import "./IHelloWorldServiceManager.sol";
+
 pragma solidity ^0.8.13;
 
-contract BlockPostServiceManager {
+contract BlockPost is is 
+    ECDSAServiceManagerBase,
+    IHelloWorldServiceManager,
+    Pausable {
     uint256 private messageId;
     mapping(uint256 => string) public messages;
 
     event MessageStored(uint256 indexed messageId, string message);
+    event MessageSubmitted(uint256 indexed messageId, string message);
+    event MessageValidated(uint256 indexed messageId, string message);
 
-    constructor() {
-        messageId = 1;
+    constructor(
+        address _avsDirectory,
+        address _stakeRegistry,
+        address _delegationManager
+    )
+        ECDSAServiceManagerBase(
+            _avsDirectory,
+            _stakeRegistry,
+            address(0), // hello-world doesn't need to deal with payments
+            _delegationManager
+        )
+    {
+        messageId = 1
+
+    }
+
+    modifier onlyOperator() {
+        require(
+            registryCoordinator.getOperatorStatus(msg.sender) == 
+            IRegistryCoordinator.OperatorStatus.REGISTERED, 
+            "Caller is not a registered operator"
+        );
+        _;
     }
 
     function storeMessage(string memory _message) public returns (uint256) {
