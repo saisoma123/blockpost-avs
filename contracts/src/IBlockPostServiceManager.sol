bb0cf4b96f02f6a9a7d0556b55af2ba8739800d4
saisoma123 <suryasai.somayyajula@gmail.com>
2024-07-05 19:45:04 -0500

    Added BlockPost interface for defining AVS functions, events, and types

---
+++ contracts/src/IBlockPostServiceManager.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IBlockPostServiceManager {
+    // EVENTS
+    event NewTaskCreated(uint32 indexed taskIndex, Task task);
+
+    event TaskResponded(uint32 indexed taskIndex, Task task, address operator);
+
+    // STRUCTS
+    struct Task {
+        string name;
+        uint32 taskCreatedBlock;
+    }
+
+    function createNewTask(string memory name) external;
+
+    // NOTE: this function is called by operators to respond to a task.
+    function respondToTask(
+        Task calldata task,
+        uint32 referenceTaskIndex,
+        bytes calldata signature
+    ) external;
+}
