321fa5caff523d1a0d4614d81338f1e2a56c2ebb
saisoma123 <suryasai.somayyajula@gmail.com>
2024-07-17 15:01:08 -0500

    Added operator deployment script and imported AVS services

--- operator/go.mod
+++ operator/go.mod
@@ -3,7 +3,13 @@ module operator
 go 1.22.4
 
 require (
-	github.com/Layr-Labs/eigensdk-go v0.1.8 // indirect
+	github.com/Layr-Labs/eigensdk-go v0.1.8
+	github.com/Layr-Labs/incredible-squaring-avs v0.0.0-20240514045747-90d3254784d9
+	github.com/ethereum/go-ethereum v1.14.6
+	github.com/prometheus/client_golang v1.19.0
+)
+
+require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
@@ -26,11 +32,11 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
-	github.com/ethereum/go-ethereum v1.14.6 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
@@ -39,14 +45,15 @@ require (
 	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/lmittmann/tint v1.0.4 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
-	github.com/prometheus/client_golang v1.19.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/supranational/blst v0.3.11 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/urfave/cli v1.22.14 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
--- operator/operator.go
+++ operator/operator.go
@@ -3,10 +3,235 @@ package operator
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/prometheus/client_golang/prometheus"
+
+	cstaskmanager "github.com/Layr-Labs/incredible-squaring-avs/contracts/bindings/IncredibleSquaringTaskManager"
+	"github.com/Layr-Labs/incredible-squaring-avs/core/chainio"
+	"github.com/Layr-Labs/incredible-squaring-avs/metrics"
+	"github.com/Layr-Labs/incredible-squaring-avs/types"
+
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients"
+	sdkelcontracts "github.com/Layr-Labs/eigensdk-go/chainio/clients/elcontracts"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/wallet"
+	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
+	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
+	sdkecdsa "github.com/Layr-Labs/eigensdk-go/crypto/ecdsa"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	sdklogging "github.com/Layr-Labs/eigensdk-go/logging"
+	sdkmetrics "github.com/Layr-Labs/eigensdk-go/metrics"
+	"github.com/Layr-Labs/eigensdk-go/metrics/collectors/economic"
+	rpccalls "github.com/Layr-Labs/eigensdk-go/metrics/collectors/rpc_calls"
+	"github.com/Layr-Labs/eigensdk-go/nodeapi"
+	"github.com/Layr-Labs/eigensdk-go/signerv2"
+	sdktypes "github.com/Layr-Labs/eigensdk-go/types"
 )
+
+const AVS_NAME = "blockpost-avs"
+const SEM_VER = "0.0.1"
+
+type Operator struct {
+	config    types.NodeConfig
+	logger    logging.Logger
+	ethClient eth.Client
+	// TODO(samlaf): remove both avsWriter and eigenlayerWrite from operator
+	// they are only used for registration, so we should make a special registration package
+	// this way, auditing this operator code makes it obvious that operators don't need to
+	// write to the chain during the course of their normal operations
+	// writing to the chain should be done via the cli only
+	metricsReg       *prometheus.Registry
+	metrics          metrics.Metrics
+	nodeApi          *nodeapi.NodeApi
+	avsWriter        *chainio.AvsWriter
+	avsReader        chainio.AvsReaderer
+	avsSubscriber    chainio.AvsSubscriberer
+	eigenlayerReader sdkelcontracts.ELReader
+	eigenlayerWriter sdkelcontracts.ELWriter
+	blsKeypair       *bls.KeyPair
+	operatorId       sdktypes.OperatorId
+	operatorAddr     common.Address
+	// receive new tasks in this chan (typically from listening to onchain event)
+	newTaskCreatedChan chan *cstaskmanager.ContractIncredibleSquaringTaskManagerNewTaskCreated
+	// needed when opting in to avs (allow this service manager contract to slash operator)
+	credibleSquaringServiceManagerAddr common.Address
+}
+
+func NewOperatorFromConfig(c types.NodeConfig) (*Operator, error) {
+	var logLevel logging.LogLevel
+	if c.Production {
+		logLevel = sdklogging.Production
+	} else {
+		logLevel = sdklogging.Development
+	}
+	logger, err := sdklogging.NewZapLogger(logLevel)
+	if err != nil {
+		return nil, err
+	}
+	reg := prometheus.NewRegistry()
+	eigenMetrics := sdkmetrics.NewEigenMetrics(AVS_NAME, c.EigenMetricsIpPortAddress, reg, logger)
+	avsAndEigenMetrics := metrics.NewAvsAndEigenMetrics(AVS_NAME, eigenMetrics, reg)
+
+	// Setup Node Api
+	nodeApi := nodeapi.NewNodeApi(AVS_NAME, SEM_VER, c.NodeApiIpPortAddress, logger)
+
+	var ethRpcClient, ethWsClient eth.Client
+	if c.EnableMetrics {
+		rpcCallsCollector := rpccalls.NewCollector(AVS_NAME, reg)
+		ethRpcClient, err = eth.NewInstrumentedClient(c.EthRpcUrl, rpcCallsCollector)
+		if err != nil {
+			logger.Errorf("Cannot create http ethclient", "err", err)
+			return nil, err
+		}
+		ethWsClient, err = eth.NewInstrumentedClient(c.EthWsUrl, rpcCallsCollector)
+		if err != nil {
+			logger.Errorf("Cannot create ws ethclient", "err", err)
+			return nil, err
+		}
+	} else {
+		ethRpcClient, err = eth.NewClient(c.EthRpcUrl)
+		if err != nil {
+			logger.Errorf("Cannot create http ethclient", "err", err)
+			return nil, err
+		}
+		ethWsClient, err = eth.NewClient(c.EthWsUrl)
+		if err != nil {
+			logger.Errorf("Cannot create ws ethclient", "err", err)
+			return nil, err
+		}
+	}
+
+	blsKeyPassword, ok := os.LookupEnv("OPERATOR_BLS_KEY_PASSWORD")
+	if !ok {
+		logger.Warnf("OPERATOR_BLS_KEY_PASSWORD env var not set. using empty string")
+	}
+	blsKeyPair, err := bls.ReadPrivateKeyFromFile(c.BlsPrivateKeyStorePath, blsKeyPassword)
+	if err != nil {
+		logger.Errorf("Cannot parse bls private key", "err", err)
+		return nil, err
+	}
+	// TODO(samlaf): should we add the chainId to the config instead?
+	// this way we can prevent creating a signer that signs on mainnet by mistake
+	// if the config says chainId=5, then we can only create a goerli signer
+	chainId, err := ethRpcClient.ChainID(context.Background())
+	if err != nil {
+		logger.Error("Cannot get chainId", "err", err)
+		return nil, err
+	}
+
+	ecdsaKeyPassword, ok := os.LookupEnv("OPERATOR_ECDSA_KEY_PASSWORD")
+	if !ok {
+		logger.Warnf("OPERATOR_ECDSA_KEY_PASSWORD env var not set. using empty string")
+	}
+
+	signerV2, _, err := signerv2.SignerFromConfig(signerv2.Config{
+		KeystorePath: c.EcdsaPrivateKeyStorePath,
+		Password:     ecdsaKeyPassword,
+	}, chainId)
+	if err != nil {
+		panic(err)
+	}
+	chainioConfig := clients.BuildAllConfig{
+		EthHttpUrl:                 c.EthRpcUrl,
+		EthWsUrl:                   c.EthWsUrl,
+		RegistryCoordinatorAddr:    c.AVSRegistryCoordinatorAddress,
+		OperatorStateRetrieverAddr: c.OperatorStateRetrieverAddress,
+		AvsName:                    AVS_NAME,
+		PromMetricsIpPortAddress:   c.EigenMetricsIpPortAddress,
+	}
+	operatorEcdsaPrivateKey, err := sdkecdsa.ReadKey(
+		c.EcdsaPrivateKeyStorePath,
+		ecdsaKeyPassword,
+	)
+	if err != nil {
+		return nil, err
+	}
+	sdkClients, err := clients.BuildAll(chainioConfig, operatorEcdsaPrivateKey, logger)
+	if err != nil {
+		panic(err)
+	}
+	skWallet, err := wallet.NewPrivateKeyWallet(ethRpcClient, signerV2, common.HexToAddress(c.OperatorAddress), logger)
+	if err != nil {
+		panic(err)
+	}
+	txMgr := txmgr.NewSimpleTxManager(skWallet, ethRpcClient, logger, common.HexToAddress(c.OperatorAddress))
+
+	avsWriter, err := chainio.BuildAvsWriter(
+		txMgr, common.HexToAddress(c.AVSRegistryCoordinatorAddress),
+		common.HexToAddress(c.OperatorStateRetrieverAddress), ethRpcClient, logger,
+	)
+	if err != nil {
+		logger.Error("Cannot create AvsWriter", "err", err)
+		return nil, err
+	}
+
+	avsReader, err := chainio.BuildAvsReader(
+		common.HexToAddress(c.AVSRegistryCoordinatorAddress),
+		common.HexToAddress(c.OperatorStateRetrieverAddress),
+		ethRpcClient, logger)
+	if err != nil {
+		logger.Error("Cannot create AvsReader", "err", err)
+		return nil, err
+	}
+	avsSubscriber, err := chainio.BuildAvsSubscriber(common.HexToAddress(c.AVSRegistryCoordinatorAddress),
+		common.HexToAddress(c.OperatorStateRetrieverAddress), ethWsClient, logger,
+	)
+	if err != nil {
+		logger.Error("Cannot create AvsSubscriber", "err", err)
+		return nil, err
+	}
+
+	// We must register the economic metrics separately because they are exported metrics (from jsonrpc or subgraph calls)
+	// and not instrumented metrics: see https://prometheus.io/docs/instrumenting/writing_clientlibs/#overall-structure
+	quorumNames := map[sdktypes.QuorumNum]string{
+		0: "quorum0",
+	}
+	economicMetricsCollector := economic.NewCollector(
+		sdkClients.ElChainReader, sdkClients.AvsRegistryChainReader,
+		AVS_NAME, logger, common.HexToAddress(c.OperatorAddress), quorumNames)
+	reg.MustRegister(economicMetricsCollector)
+
+	operator := &Operator{
+		config:                             c,
+		logger:                             logger,
+		metricsReg:                         reg,
+		metrics:                            avsAndEigenMetrics,
+		nodeApi:                            nodeApi,
+		ethClient:                          ethRpcClient,
+		avsWriter:                          avsWriter,
+		avsReader:                          avsReader,
+		avsSubscriber:                      avsSubscriber,
+		eigenlayerReader:                   sdkClients.ElChainReader,
+		eigenlayerWriter:                   sdkClients.ElChainWriter,
+		blsKeypair:                         blsKeyPair,
+		operatorAddr:                       common.HexToAddress(c.OperatorAddress),
+		newTaskCreatedChan:                 make(chan *cstaskmanager.ContractIncredibleSquaringTaskManagerNewTaskCreated),
+		credibleSquaringServiceManagerAddr: common.HexToAddress(c.AVSRegistryCoordinatorAddress),
+		operatorId:                         [32]byte{0}, // this is set below
+
+	}
+
+	// OperatorId is set in contract during registration so we get it after registering operator.
+	operatorId, err := sdkClients.AvsRegistryChainReader.GetOperatorId(&bind.CallOpts{}, operator.operatorAddr)
+	if err != nil {
+		logger.Error("Cannot get operator id", "err", err)
+		return nil, err
+	}
+	operator.operatorId = operatorId
+	logger.Info("Operator info",
+		"operatorId", operatorId,
+		"operatorAddr", c.OperatorAddress,
+		"operatorG1Pubkey", operator.blsKeypair.GetPubKeyG1(),
+		"operatorG2Pubkey", operator.blsKeypair.GetPubKeyG2(),
+	)
+
+	return operator, nil
+
+}
 
 func (o *Operator) ProcessNewMessageLog(newMessageLog *blockpost.ContractBlockPostMessageSubmitted) *blockpost.ContractBlockPostValidatedMessage {
 	o.logger.Debug("Received new message", "message", newMessageLog)
