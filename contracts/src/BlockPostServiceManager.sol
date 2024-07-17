f0d53ef774b55ac4e7f980d136dd13557541ef5a
saisoma123 <suryasai.somayyajula@gmail.com>
2024-07-16 19:34:06 -0500

    Updated deprecated methods within ServiceManager for compilation

--- contracts/src/BlockPostServiceManager.sol
+++ contracts/src/BlockPostServiceManager.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "eigenlayer/contracts/libraries/BytesLib.sol";
-import "eigenlayer/contracts/core/DelegationManager.sol";
+import "@eigenlayer/contracts/libraries/BytesLib.sol";
+import "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
+import "@eigenlayer/contracts/core/DelegationManager.sol";
 import "eigenlayer-middleware/src/ServiceManagerBase.sol";
 import "eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
-import "openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
-import "eigenlayer/contracts/permissions/Pausable.sol";
+import "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
+import "@eigenlayer/contracts/permissions/Pausable.sol";
 import {IRegistryCoordinator} from "eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
-import "./IBlockPostServiceManager.sol";
 
 pragma solidity ^0.8.13;
 
-contract BlockPostServiceManager is
-    ServiceManagerBase,
-    IHelloWorldServiceManager,
-    Pausable
-{
+contract BlockPostServiceManager is ServiceManagerBase, Pausable {
     uint256 private messageId;
+    IStakeRegistry private stakeRegistry;
 
     mapping(uint256 => string) public messages;
-    // mapping(uint256 => bool) public messageValidated;
+    mapping(uint256 => bool) public messageValidated;
     mapping(uint256 => bytes) public messageSignatures;
 
     event MessageStored(uint256 indexed messageId, string message);
@@ -28,61 +25,63 @@ contract BlockPostServiceManager is
     event MessageValidated(uint256 indexed messageId, string message);
 
     constructor(
-        address _avsDirectory,
-        address _stakeRegistry,
-        address _delegationManager
-    )
-        ECDSAServiceManagerBase(
-            _avsDirectory,
-            _stakeRegistry,
-            address(0),
-            _delegationManager
-        )
-    {
+        IAVSDirectory _avsDirectory,
+        IRegistryCoordinator _registryCoordinator,
+        IStakeRegistry _stakeRegistry
+    ) ServiceManagerBase(_avsDirectory, _registryCoordinator, _stakeRegistry) {
+        stakeRegistry = _stakeRegistry;
         messageId = 1;
     }
 
+    /** 
     modifier onlyOperator() {
         require(
-            registryCoordinator.getOperatorStatus(msg.sender) ==
-                IRegistryCoordinator.OperatorStatus.REGISTERED,
+            IStakeRegistry(stakeRegistry).operatorRegistered(msg.sender),
             "Caller is not a registered operator"
         );
         _;
     }
+    */
 
     function submitMessage(
         string memory _message
     ) public whenNotPaused returns (uint256) {
         require(bytes(_message).length > 0, "Message cannot be empty");
 
-        uint256 messageId = latestMessageId;
-        latestMessageId++;
+        uint256 newMessageId = messageId;
+        messageId++;
 
-        emit MessageSubmitted(messageId, _message);
-        return messageId;
+        emit MessageSubmitted(newMessageId, _message);
+        return newMessageId;
     }
 
     function storeValidatedMessage(
         uint256 _messageId,
         string memory _message,
         bytes memory _signature
-    ) public onlyOperator whenNotPaused {
-        //require(bytes(_message).length > 0, "Message cannot be empty");
+    ) public whenNotPaused {
+        require(bytes(_message).length > 0, "Message cannot be empty");
         require(!messageValidated[_messageId], "Message already validated");
 
         // Verify the signature
         bytes32 messageHash = keccak256(abi.encodePacked(_message));
-        bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
-        address signer = ethSignedMessageHash.recover(_signature);
+        bytes32 ethSignedMessageHash = ECDSAUpgradeable.toEthSignedMessageHash(
+            messageHash
+        );
+        address signer = ECDSAUpgradeable.recover(
+            ethSignedMessageHash,
+            _signature
+        );
 
-        require(operators[signer], "Invalid signature");
+        require(signer == msg.sender, "Message signer is not operator");
+
+        //require(operators[signer], "Invalid signature");
 
         messages[_messageId] = _message;
-        // messageValidated[_messageId] = true;
+        messageValidated[_messageId] = true;
         messageSignatures[_messageId] = _signature;
 
-        emit MessageValidated(_messageId, _message, _signature);
+        emit MessageValidated(_messageId, _message);
     }
 
     /** 
