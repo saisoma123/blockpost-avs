14c78a0b4c64ac564c1ab50831cc2035bed13001
saisoma123 <suryasai.somayyajula@gmail.com>
2024-07-26 14:43:33 -0500

    Added more comprehensive testing

--- contracts/test/BlockPostServiceManager.t.sol
+++ contracts/test/BlockPostServiceManager.t.sol
@@ -123,6 +123,105 @@ contract BlockPostServiceManagerTest is Test {
         );
     }
 
+    function testRetrieveMessageNotStored() public {
+        vm.stopPrank();
+        vm.startPrank(user);
+        messageId = blockPostServiceManager.submitMessage("Hello, World!");
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        vm.expectRevert("Message ID does not exist");
+        blockPostServiceManager.retrieveMessage(messageId);
+    }
+
+    function testRetrieveMessageNotOwner() public {
+        vm.stopPrank();
+        vm.prank(user);
+        messageId = blockPostServiceManager.submitMessage("Hello, World!");
+        vm.stopPrank();
+
+        vm.prank(validator);
+        (messageHash, signature) = signMessage("Hello, World!");
+
+        blockPostServiceManager.storeValidatedMessage(
+            messageId,
+            "Hello, World!",
+            signature
+        );
+        vm.stopPrank();
+
+        vm.expectRevert("This is not your message!");
+        vm.prank(owner);
+        blockPostServiceManager.retrieveMessage(messageId);
+    }
+
+    function testRetrieveMessageWithInvalidSignature() public {
+        vm.stopPrank();
+        vm.startPrank(user);
+        messageId = blockPostServiceManager.submitMessage("Hello, World!");
+        vm.stopPrank();
+
+        vm.startPrank(validator);
+        (messageHash, signature) = signMessage("Invalid Message");
+        vm.expectRevert("Message signer is not operator");
+        blockPostServiceManager.storeValidatedMessage(
+            messageId,
+            "Hello, World!",
+            signature
+        );
+    }
+
+    function testRetrieveMultipleMessages() public {
+        vm.stopPrank();
+        vm.startPrank(user);
+
+        uint256[] memory messageIds = new uint256[](15);
+        string[15] memory messages = [
+            "Message 1",
+            "Message 2",
+            "Message 3",
+            "Message 4",
+            "Message 5",
+            "Message 6",
+            "Message 7",
+            "Message 8",
+            "Message 9",
+            "Message 10",
+            "Message 11",
+            "Message 12",
+            "Message 13",
+            "Message 14",
+            "Message 15"
+        ];
+
+        // Submit 15 messages
+        for (uint i = 0; i < 15; i++) {
+            messageIds[i] = blockPostServiceManager.submitMessage(messages[i]);
+        }
+        vm.stopPrank();
+
+        vm.startPrank(validator);
+        // Validate 15 messages
+        for (uint i = 0; i < 15; i++) {
+            (messageHash, signature) = signMessage(messages[i]);
+            blockPostServiceManager.storeValidatedMessage(
+                messageIds[i],
+                messages[i],
+                signature
+            );
+        }
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        // Retrieve and check 15 messages
+        for (uint i = 0; i < 15; i++) {
+            string memory retrievedMessage = blockPostServiceManager
+                .retrieveMessage(messageIds[i]);
+            assertEq(retrievedMessage, messages[i]);
+        }
+        vm.stopPrank();
+    }
+
     function signMessage(
         string memory _message
     ) internal view returns (bytes32, bytes memory) {
