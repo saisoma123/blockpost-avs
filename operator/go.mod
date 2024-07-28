d7ae30fff839ede7e9ea8765daaebf7d8a85b6d0
saisoma123 <suryasai.somayyajula@gmail.com>
2024-07-27 21:03:46 -0500

    Added operator tests to check off chain validation and smart contract interaction

--- operator/go.mod
+++ operator/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/Layr-Labs/incredible-squaring-avs v0.0.0-20240514045747-90d3254784d9
 	github.com/ethereum/go-ethereum v1.14.6
 	github.com/prometheus/client_golang v1.19.0
+	github.com/stretchr/testify v1.9.0
+	github.com/urfave/cli v1.22.14
 )
 
 require (
@@ -34,6 +36,7 @@ require (
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
@@ -45,16 +48,15 @@ require (
 	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/lmittmann/tint v1.0.4 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
-	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
 	github.com/supranational/blst v0.3.11 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
-	github.com/urfave/cli v1.22.14 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
---
+++ operator/operator_test.go
@@ -0,0 +1,725 @@
+package operator
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/assert"
+)
+
+const contractABI = `[
+  {
+    "type": "constructor",
+    "inputs": [
+      {
+        "name": "_avsDirectory",
+        "type": "address",
+        "internalType": "contract IAVSDirectory"
+      },
+      {
+        "name": "_registryCoordinator",
+        "type": "address",
+        "internalType": "contract IRegistryCoordinator"
+      },
+      {
+        "name": "_stakeRegistry",
+        "type": "address",
+        "internalType": "contract IStakeRegistry"
+      }
+    ],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "avsDirectory",
+    "inputs": [],
+    "outputs": [
+      {
+        "name": "",
+        "type": "address",
+        "internalType": "address"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "deregisterOperatorFromAVS",
+    "inputs": [
+      {
+        "name": "operator",
+        "type": "address",
+        "internalType": "address"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "getOperatorRestakedStrategies",
+    "inputs": [
+      {
+        "name": "operator",
+        "type": "address",
+        "internalType": "address"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]",
+        "internalType": "address[]"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getRestakeableStrategies",
+    "inputs": [],
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]",
+        "internalType": "address[]"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "messageSignatures",
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "bytes",
+        "internalType": "bytes"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "messageValidated",
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool",
+        "internalType": "bool"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "messages",
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "string",
+        "internalType": "string"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "owner",
+    "inputs": [],
+    "outputs": [
+      {
+        "name": "",
+        "type": "address",
+        "internalType": "address"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "pause",
+    "inputs": [
+      {
+        "name": "newPausedStatus",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "pauseAll",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "paused",
+    "inputs": [
+      {
+        "name": "index",
+        "type": "uint8",
+        "internalType": "uint8"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool",
+        "internalType": "bool"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "paused",
+    "inputs": [],
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "pauserRegistry",
+    "inputs": [],
+    "outputs": [
+      {
+        "name": "",
+        "type": "address",
+        "internalType": "contract IPauserRegistry"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "registerOperatorToAVS",
+    "inputs": [
+      {
+        "name": "operator",
+        "type": "address",
+        "internalType": "address"
+      },
+      {
+        "name": "operatorSignature",
+        "type": "tuple",
+        "internalType": "struct ISignatureUtils.SignatureWithSaltAndExpiry",
+        "components": [
+          {
+            "name": "signature",
+            "type": "bytes",
+            "internalType": "bytes"
+          },
+          {
+            "name": "salt",
+            "type": "bytes32",
+            "internalType": "bytes32"
+          },
+          {
+            "name": "expiry",
+            "type": "uint256",
+            "internalType": "uint256"
+          }
+        ]
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "renounceOwnership",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "retrieveMessage",
+    "inputs": [
+      {
+        "name": "_messageId",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "string",
+        "internalType": "string"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "setPauserRegistry",
+    "inputs": [
+      {
+        "name": "newPauserRegistry",
+        "type": "address",
+        "internalType": "contract IPauserRegistry"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "storeValidatedMessage",
+    "inputs": [
+      {
+        "name": "_messageId",
+        "type": "uint256",
+        "internalType": "uint256"
+      },
+      {
+        "name": "_message",
+        "type": "string",
+        "internalType": "string"
+      },
+      {
+        "name": "_signature",
+        "type": "bytes",
+        "internalType": "bytes"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "submitMessage",
+    "inputs": [
+      {
+        "name": "_message",
+        "type": "string",
+        "internalType": "string"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "transferOwnership",
+    "inputs": [
+      {
+        "name": "newOwner",
+        "type": "address",
+        "internalType": "address"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "unpause",
+    "inputs": [
+      {
+        "name": "newPausedStatus",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "updateAVSMetadataURI",
+    "inputs": [
+      {
+        "name": "_metadataURI",
+        "type": "string",
+        "internalType": "string"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "event",
+    "name": "Initialized",
+    "inputs": [
+      {
+        "name": "version",
+        "type": "uint8",
+        "indexed": false,
+        "internalType": "uint8"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "MessageStored",
+    "inputs": [
+      {
+        "name": "messageId",
+        "type": "uint256",
+        "indexed": true,
+        "internalType": "uint256"
+      },
+      {
+        "name": "message",
+        "type": "string",
+        "indexed": false,
+        "internalType": "string"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "MessageSubmitted",
+    "inputs": [
+      {
+        "name": "messageId",
+        "type": "uint256",
+        "indexed": true,
+        "internalType": "uint256"
+      },
+      {
+        "name": "message",
+        "type": "string",
+        "indexed": false,
+        "internalType": "string"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "MessageValidated",
+    "inputs": [
+      {
+        "name": "messageId",
+        "type": "uint256",
+        "indexed": true,
+        "internalType": "uint256"
+      },
+      {
+        "name": "message",
+        "type": "string",
+        "indexed": false,
+        "internalType": "string"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "OwnershipTransferred",
+    "inputs": [
+      {
+        "name": "previousOwner",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "newOwner",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "Paused",
+    "inputs": [
+      {
+        "name": "account",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "newPausedStatus",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "PauserRegistrySet",
+    "inputs": [
+      {
+        "name": "pauserRegistry",
+        "type": "address",
+        "indexed": false,
+        "internalType": "contract IPauserRegistry"
+      },
+      {
+        "name": "newPauserRegistry",
+        "type": "address",
+        "indexed": false,
+        "internalType": "contract IPauserRegistry"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "Unpaused",
+    "inputs": [
+      {
+        "name": "account",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "newPausedStatus",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  }
+];`
+
+func TestVerifyMessageIntegrity(t *testing.T) {
+	message := "Test Message"
+	expectedHash := crypto.Keccak256Hash([]byte(message)).Bytes()
+
+	isValid := VerifyMessageIntegrity(message, expectedHash)
+	assert.True(t, isValid)
+
+	invalidHash := crypto.Keccak256Hash([]byte("Different Message")).Bytes()
+	isValid = VerifyMessageIntegrity(message, invalidHash)
+	assert.False(t, isValid)
+}
+
+func TestSignValidatedMessage(t *testing.T) {
+
+	validatedMessage := ValidatedMessage{
+		MessageId:   big.NewInt(1),
+		Message:     "Test Message",
+		messageHash: ComputeMessageHash("Test Message"),
+	}
+
+	isValid := VerifyMessageIntegrity(validatedMessage.Message, validatedMessage.messageHash)
+	if !isValid {
+		fmt.Println("Message integrity verification failed", "messageId", validatedMessage.MessageId)
+
+	}
+
+	ethHash := toEthSignedMessageHash(validatedMessage.messageHash)
+
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf("Failed to generate private key: %v", err)
+	}
+
+	signature, err := crypto.Sign(ethHash, privateKey)
+	if err != nil {
+		fmt.Println("Failed to sign validated message", "err", err)
+	}
+
+	if signature[64] < 27 {
+		signature[64] += 27
+	}
+
+	signedMessage := &SignedMessage{
+		MessageId: validatedMessage.MessageId,
+		Message:   validatedMessage.Message,
+		Signature: signature,
+	}
+
+	assert.NotNil(t, signedMessage)
+	assert.Equal(t, validatedMessage.MessageId, signedMessage.MessageId)
+	assert.Equal(t, validatedMessage.Message, signedMessage.Message)
+	assert.NotNil(t, signedMessage.Signature)
+}
+
+const privateKeyHex = ""
+
+func TestSmartContractInteraction(t *testing.T) {
+	client, err := ethclient.Dial("https://ethereum-holesky-rpc.publicnode.com")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	contractAddress := common.HexToAddress("0xc5eFF99FB98b1eBEEf2533b30e60ba72f1FA28B3")
+	parsedABI, err := abi.JSON(strings.NewReader(contractABI))
+	if err != nil {
+		log.Fatalf("Failed to parse ABI: %v", err)
+	}
+
+	message := "Hello, Blockchain!"
+	callData, err := parsedABI.Pack("submitMessage", message)
+	if err != nil {
+		log.Fatalf("Failed to pack method call: %v", err)
+	}
+
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		log.Fatalf("Failed to parse private key: %v", err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatalf("Cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatalf("Failed to get nonce: %v", err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to suggest gas price: %v", err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(17000))
+	if err != nil {
+		log.Fatalf("Failed to create keyed transactor: %v", err)
+	}
+
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)      // in wei
+	auth.GasLimit = uint64(3000000) // in units
+	auth.GasPrice = gasPrice
+
+	tx := types.NewTransaction(nonce, contractAddress, big.NewInt(0), auth.GasLimit, gasPrice, callData)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get chain ID: %v", err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatalf("Failed to sign transaction: %v", err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatalf("Failed to send transaction: %v", err)
+	}
+
+	fmt.Printf("Transaction sent: %s\n", signedTx.Hash().Hex())
+
+	// Now call the contract method to get the return value
+	var result *big.Int
+	callMsg := ethereum.CallMsg{
+		From: fromAddress,
+		To:   &contractAddress,
+		Data: callData,
+	}
+
+	res, err := client.CallContract(context.Background(), callMsg, nil)
+	if err != nil {
+		log.Fatalf("Failed to call contract method: %v", err)
+	}
+
+	err = parsedABI.UnpackIntoInterface(&result, "submitMessage", res)
+	if err != nil {
+		log.Fatalf("Failed to unpack result: %v", err)
+	}
+
+	fmt.Printf("Return value: %s\n", result.String())
+
+	// Convert result to uint256 and call retrieveMessage function
+	uint256Result := new(big.Int).Set(result)
+	callDataRetrieve, err := parsedABI.Pack("retrieveMessage", uint256Result)
+	if err != nil {
+		log.Fatalf("Failed to pack retrieveMessage call: %v", err)
+	}
+
+	maxAttempts := 10
+	var retrievedMessage string
+	for i := 0; i < maxAttempts; i++ {
+		time.Sleep(5 * time.Second)
+
+		callMsgRetrieve := ethereum.CallMsg{
+			From: fromAddress,
+			To:   &contractAddress,
+			Data: callDataRetrieve,
+		}
+
+		resRetrieve, err := client.CallContract(context.Background(), callMsgRetrieve, nil)
+		if err != nil {
+			log.Printf("Failed to call retrieveMessage method: %v", err)
+			continue
+		}
+
+		err = parsedABI.UnpackIntoInterface(&retrievedMessage, "retrieveMessage", resRetrieve)
+		if err != nil {
+			log.Printf("Failed to unpack retrieveMessage result: %v", err)
+			continue
+		}
+
+		if retrievedMessage == message {
+			fmt.Printf("Retrieved message: %s\n", retrievedMessage)
+			fmt.Println("Retrieved message matches the original message")
+			return
+		}
+		log.Printf("Attempt %d: Retrieved message does not match the original message", i+1)
+	}
+
+	log.Fatalf("Failed to retrieve the correct message after %d attempts", maxAttempts)
+}
